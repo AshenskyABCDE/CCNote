ab92da0e8d409dc8b61e27ca8f066b6be1fe05e5
ashensky <251902756@qq.com>
2024-06-14 23:52:46 +0800

    改掉bug

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -15,4 +15,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface IShopService extends IService<Shop> {
 
     Result queryById(Long id);
+    Result update(Shop shop);
 }
--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -10,6 +10,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import io.netty.util.internal.StringUtil;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -46,5 +47,19 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop));
         // 数据库存在
         return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        // 更新数据库
+        updateById(shop);
+        Long id = shop.getId();
+        if (id == null) {
+            return Result.fail("id不要输入空");
+        }
+        // 删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + shop.getId());
+        return Result.ok();
     }
 }
